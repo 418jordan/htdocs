528e1ad0f1876ce1726438040d476f47a76254b8
418jordan <418jordan>
2016-10-13 21:57:09 -0600

    updates 10/13/16 10pm mst
    updates 10/13/16 10pm mst

--- busybeephotography/busybee.php
+++ busybeephotography/busybee.php
@@ -55,7 +55,7 @@
         <div class="container text-center">    
 
                 <div >
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <div id="myCarousel" class="carousel slide" style="max-height: 200px;" data-ride="carousel">
                         <!-- Indicators -->
                         <ol class="carousel-indicators">
                             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -65,7 +65,7 @@
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" role="listbox">
                             <div class="item active">
-                                <img src="https://placehold.it/800x400?text=IMAGE" alt="Image">
+                                <img src="myportfolio/images/flower1.JPG" alt="Image">
                                 <div class="carousel-caption">
                                     <h3>Sell $</h3>
                                     <p>Money Money.</p>
@@ -73,7 +73,129 @@
                             </div>
 
                             <div class="item">
-                                <img src="https://placehold.it/800x400?text=Another Image Maybe" alt="Image">
+                                <img src="myportfolio/images/hawaii.jpg" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>More Sell $</h3>
+                                    <p>Lorem ipsum...</p>
+                                </div>      
+                            </div>
+                        </div>
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+
+                <hr>
+            </div>
+          <div class="container text-center">    
+
+                <div >
+                    <div id="myCarousel" class="carousel slide" style="max-height: 200px;" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="myportfolio/images/flower1.JPG" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>Sell $</h3>
+                                    <p>Money Money.</p>
+                                </div>      
+                            </div>
+
+                            <div class="item">
+                                <img src="myportfolio/images/hawaii.jpg" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>More Sell $</h3>
+                                    <p>Lorem ipsum...</p>
+                                </div>      
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+
+                <hr>
+            </div>
+          <div class="container text-center">    
+
+                <div >
+                    <div id="myCarousel" class="carousel slide" style="max-height: 200px;" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="../myportfolio/images/flower1.JPG" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>Sell $</h3>
+                                    <p>Money Money.</p>
+                                </div>      
+                            </div>
+
+                            <div class="item">
+                                <img src="../myportfolio/images/flower1.JPG" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>More Sell $</h3>
+                                    <p>Lorem ipsum...</p>
+                                </div>      
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+
+                <hr>
+            </div>
+          <div class="container text-center">    
+
+                <div >
+                    <div id="myCarousel" class="carousel slide" style="max-height: 200px;" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
+
+                         <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="myportfolio/images/flower1.JPG" alt="Image">
+                                <div class="carousel-caption">
+                                    <h3>Sell $</h3>
+                                    <p>Money Money.</p>
+                                </div>      
+                            </div>
+
+                            <div class="item">
+                                <img src="myportfolio/images/hawaii.jpg" alt="Image">
                                 <div class="carousel-caption">
                                     <h3>More Sell $</h3>
                                     <p>Lorem ipsum...</p>
